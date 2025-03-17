93a073624f638c5623237cbc73abce80cd169d21
Mike <ban.mihaiconstantin@gmail.com>
2025-03-17 22:26:03 +0200

    doesnt work yet

--- LoanShark/LoanShark/View/LoanView.xaml.cs
+++ LoanShark/LoanShark/View/LoanView.xaml.cs
@@ -5,228 +5,232 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using LoanShark.ViewModel;
+using System;
+using System.Diagnostics;
 
 namespace LoanShark
 {
     public sealed partial class LoanView : Window
     {
-        public ObservableCollection<Loan> Loans { get; set; }
-
-        private LoanViewModel loansViewModel;
+        private readonly LoanViewModel DataContext;
 
         public LoanView(int userID)
         {
+            this.DataContext = new LoanViewModel(userID);
             this.InitializeComponent();
-            loansViewModel = new LoanViewModel(userID);
-            Loans = new ObservableCollection<Loan>(GetUnpaidLoans().OrderBy(loan => loan.DateTaken));
-            LoansListView.ItemsSource = Loans;
-            InitializeTakeLoanPage();
-            InitializePayLoanPage();
+            //Loans = new ObservableCollection<Loan>(GetUnpaidLoans().OrderBy(loan => loan.DateTaken));
+            //LoansListView.ItemsSource = Loans;
+            //InitializeTakeLoanPage();
+            //InitializePayLoanPage();
         }
 
-        private void InitializeTakeLoanPage()
-        {
-            // Populate BankAccountComboBox with aggregated bank account information
-            BankAccountComboBox.ItemsSource = GetBankAccounts();
+        //private void ShowPage(string pageName)
+        //{
+        //    MainLoansPage.Visibility = pageName == "MainLoansPage" ? Visibility.Visible : Visibility.Collapsed;
+        //    TakeLoanPage.Visibility = pageName == "TakeLoanPage" ? Visibility.Visible : Visibility.Collapsed;
+        //    PayLoanPage.Visibility = pageName == "PayLoanPage" ? Visibility.Visible : Visibility.Collapsed;
+        //}
 
-            // Populate MonthsComboBox with possible number of months
-            MonthsComboBox.ItemsSource = new List<int> { 6, 12, 24, 36 };
-        }
+        //private void GoToTakeLoanPageButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ShowPage("TakeLoanPage");
+        //}
 
-        private void InitializePayLoanPage()
-        {
-            // Populate PayBankAccountComboBox with aggregated bank account information
-            PayBankAccountComboBox.ItemsSource = GetBankAccounts();
+        //private void GoToPayLoanPageButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ShowPage("PayLoanPage");
+        //}
 
-            // Populate LoanToPayComboBox with unpaid loans
-            LoanToPayComboBox.ItemsSource = GetUnpaidLoans().Select(loan => new
-            {
-                Display = $"{loan.Currency} - {loan.AmountToPay} - {loan.DateDeadline}",
-                Loan = loan
-            }).ToList();
-            LoanToPayComboBox.DisplayMemberPath = "Display";
-            LoanToPayComboBox.SelectedValuePath = "Loan";
-        }
+        //private void CloseButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Close();
+        //}
 
-        private IEnumerable<string> GetBankAccounts()
-        {
-            // Assuming you have a method to get all bank accounts
-            return new List<string>
-            {
-                "IBAN1 - EUR - 1000",
-                "IBAN2 - USD - 2000",
-                "IBAN3 - GBP - 3000"
-            };
-        }
+        //private void BackButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ShowPage("MainLoansPage");
+        //}
 
-        private IEnumerable<Loan> getGeneratedLoans()
-        {
-            var Loans = new List<Loan>(); // GetAllLoans()
-            for (int i = 0; i < 5; i++)
-                Loans.Add(new Loan(i, i, i.ToString(), i, i));
+        //public ObservableCollection<Loan> Loans { get; set; }
 
-            return Loans;
-        }
 
-        private IEnumerable<Loan> GetAllLoans()
-        {
-            return getGeneratedLoans();
-        }
 
-        private IEnumerable<Loan> GetUnpaidLoans()
-        {
-            // Assuming you have a method to get all loans
-            var allLoans = GetAllLoans();
-            return allLoans.Where(loan => loan.State == "unpaid");
-        }
+        //private void InitializeTakeLoanPage()
+        //{
+        //    // Populate BankAccountComboBox with aggregated bank account information
+        //    BankAccountComboBox.ItemsSource = GetBankAccounts();
 
-        private void GoToTakeLoanPageButton_Click(object sender, RoutedEventArgs e)
-        {
-            ShowPage("TakeLoanPage");
-        }
+        //    // Populate MonthsComboBox with possible number of months
+        //    MonthsComboBox.ItemsSource = new List<int> { 6, 12, 24, 36 };
+        //}
 
-        private void GoToPayLoanPageButton_Click(object sender, RoutedEventArgs e)
-        {
-            ShowPage("PayLoanPage");
-        }
+        //private void InitializePayLoanPage()
+        //{
+        //    // Populate PayBankAccountComboBox with aggregated bank account information
+        //    PayBankAccountComboBox.ItemsSource = GetBankAccounts();
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+        //    // Populate LoanToPayComboBox with unpaid loans
+        //    LoanToPayComboBox.ItemsSource = GetUnpaidLoans().Select(loan => new
+        //    {
+        //        Display = $"{loan.Currency} - {loan.AmountToPay} - {loan.DateDeadline}",
+        //        Loan = loan
+        //    }).ToList();
+        //    LoanToPayComboBox.DisplayMemberPath = "Display";
+        //    LoanToPayComboBox.SelectedValuePath = "Loan";
+        //}
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
-        {
-            ShowPage("MainLoansPage");
-        }
+        //private IEnumerable<string> GetBankAccounts()
+        //{
+        //    // Assuming you have a method to get all bank accounts
+        //    return new List<string>
+        //    {
+        //        "IBAN1 - EUR - 1000",
+        //        "IBAN2 - USD - 2000",
+        //        "IBAN3 - GBP - 3000"
+        //    };
+        //}
 
-        private void ShowPage(string pageName)
-        {
-            MainLoansPage.Visibility = pageName == "MainLoansPage" ? Visibility.Visible : Visibility.Collapsed;
-            TakeLoanPage.Visibility = pageName == "TakeLoanPage" ? Visibility.Visible : Visibility.Collapsed;
-            PayLoanPage.Visibility = pageName == "PayLoanPage" ? Visibility.Visible : Visibility.Collapsed;
-        }
+        //private IEnumerable<Loan> getGeneratedLoans()
+        //{
+        //    var Loans = new List<Loan>(); // GetAllLoans()
+        //    for (int i = 0; i < 5; i++)
+        //        Loans.Add(new Loan(i, i, i.ToString(), i, i));
 
-        private void ClearTakeLoanPage()
-        {
-            BankAccountComboBox.SelectedIndex = -1;
-            AmountTextBox.Text = string.Empty;
-            MonthsComboBox.SelectedIndex = -1;
-            TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
-            AmountToPayTextBlock.Text = "Amount to Pay: N/A";
-            ErrorMessageTextBlock.Text = string.Empty;
-        }
-        private void ClearPayLoanPage()
-        {
-            PayBankAccountComboBox.SelectedIndex = -1;
-            LoanToPayComboBox.SelectedIndex = -1;
-            PayErrorMessageTextBlock.Text = string.Empty;
-        }
+        //    return Loans;
+        //}
 
-        private void BankAccountComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            UpdateAmountToPay();
-        }
+        //private IEnumerable<Loan> GetAllLoans()
+        //{
+        //    return getGeneratedLoans();
+        //}
 
-        private void MonthsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            UpdateAmountToPay();
-        }
+        //private IEnumerable<Loan> GetUnpaidLoans()
+        //{
+        //    // Assuming you have a method to get all loans
+        //    var allLoans = GetAllLoans();
+        //    return allLoans.Where(loan => loan.State == "unpaid");
+        //}
 
-        private void AmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UpdateAmountToPay();
-        }
+        //private void ClearTakeLoanPage()
+        //{
+        //    BankAccountComboBox.SelectedIndex = -1;
+        //    AmountTextBox.Text = string.Empty;
+        //    MonthsComboBox.SelectedIndex = -1;
+        //    TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
+        //    AmountToPayTextBlock.Text = "Amount to Pay: N/A";
+        //    ErrorMessageTextBlock.Text = string.Empty;
+        //}
+        //private void ClearPayLoanPage()
+        //{
+        //    PayBankAccountComboBox.SelectedIndex = -1;
+        //    LoanToPayComboBox.SelectedIndex = -1;
+        //    PayErrorMessageTextBlock.Text = string.Empty;
+        //}
 
-        private void UpdateAmountToPay()
-        {
-            if (BankAccountComboBox.SelectedIndex == -1 || string.IsNullOrEmpty(AmountTextBox.Text) || MonthsComboBox.SelectedIndex == -1)
-            {
-                TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
-                AmountToPayTextBlock.Text = "Amount to Pay: N/A";
-                return;
-            }
+        //private void BankAccountComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    UpdateAmountToPay();
+        //}
 
-            if (float.TryParse(AmountTextBox.Text, out float amount) && amount > 0 && amount <= 1000000)
-            {
-                int months = (int)MonthsComboBox.SelectedItem;
-                float taxPercentage = CalculateTaxPercentage(months);
-                float amountToPay = amount + (amount * taxPercentage / 100);
+        //private void MonthsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    UpdateAmountToPay();
+        //}
 
-                TaxPercentageTextBlock.Text = $"Tax Percentage: {taxPercentage}%";
-                AmountToPayTextBlock.Text = $"Amount to Pay: {amountToPay}";
-            }
-            else
-            {
-                TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
-                AmountToPayTextBlock.Text = "Amount to Pay: N/A";
-            }
-        }
+        //private void AmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    UpdateAmountToPay();
+        //}
 
-        private float CalculateTaxPercentage(int months)
-        {
-            // Implement your tax percentage calculation logic here
-            return months * 0.1f; // Example calculation
-        }
+        //private void UpdateAmountToPay()
+        //{
+        //    if (BankAccountComboBox.SelectedIndex == -1 || string.IsNullOrEmpty(AmountTextBox.Text) || MonthsComboBox.SelectedIndex == -1)
+        //    {
+        //        TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
+        //        AmountToPayTextBlock.Text = "Amount to Pay: N/A";
+        //        return;
+        //    }
 
-        private void TakeLoanButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (BankAccountComboBox.SelectedIndex == -1 || string.IsNullOrEmpty(AmountTextBox.Text) || MonthsComboBox.SelectedIndex == -1)
-            {
-                ErrorMessageTextBlock.Text = "Invalid data provided";
-                return;
-            }
+        //    if (float.TryParse(AmountTextBox.Text, out float amount) && amount > 0 && amount <= 1000000)
+        //    {
+        //        int months = (int)MonthsComboBox.SelectedItem;
+        //        float taxPercentage = CalculateTaxPercentage(months);
+        //        float amountToPay = amount + (amount * taxPercentage / 100);
 
-            if (float.TryParse(AmountTextBox.Text, out float amount) && amount > 0 && amount <= 1000000)
-            {
-                // Save the loan and update the bank account balance
-                SaveLoan();
-                ShowPage("MainLoansPage");
-                ClearTakeLoanPage();
-            }
-            else
-            {
-                ErrorMessageTextBlock.Text = "Invalid data provided";
-            }
-        }
+        //        TaxPercentageTextBlock.Text = $"Tax Percentage: {taxPercentage}%";
+        //        AmountToPayTextBlock.Text = $"Amount to Pay: {amountToPay}";
+        //    }
+        //    else
+        //    {
+        //        TaxPercentageTextBlock.Text = "Tax Percentage: N/A";
+        //        AmountToPayTextBlock.Text = "Amount to Pay: N/A";
+        //    }
+        //}
 
-        private void PayLoanButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (PayBankAccountComboBox.SelectedIndex == -1 || LoanToPayComboBox.SelectedIndex == -1)
-            {
-                PayErrorMessageTextBlock.Text = "Invalid data provided";
-                return;
-            }
+        //private float CalculateTaxPercentage(int months)
+        //{
+        //    // Implement your tax percentage calculation logic here
+        //    return months * 0.1f; // Example calculation
+        //}
 
-            var selectedBankAccount = PayBankAccountComboBox.SelectedItem.ToString();
-            var selectedLoan = (Loan)LoanToPayComboBox.SelectedValue;
+        //private void TakeLoanButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (BankAccountComboBox.SelectedIndex == -1 || string.IsNullOrEmpty(AmountTextBox.Text) || MonthsComboBox.SelectedIndex == -1)
+        //    {
+        //        ErrorMessageTextBlock.Text = "Invalid data provided";
+        //        return;
+        //    }
 
-            if (HasSufficientFunds(selectedBankAccount, selectedLoan))
-            {
-                PayLoan(selectedBankAccount, selectedLoan);
-                ShowPage("MainLoansPage");
-                ClearPayLoanPage();
-            }
-            else
-            {
-                PayErrorMessageTextBlock.Text = "Not enough funds";
-            }
-        }
+        //    if (float.TryParse(AmountTextBox.Text, out float amount) && amount > 0 && amount <= 1000000)
+        //    {
+        //        // Save the loan and update the bank account balance
+        //        SaveLoan();
+        //        ShowPage("MainLoansPage");
+        //        ClearTakeLoanPage();
+        //    }
+        //    else
+        //    {
+        //        ErrorMessageTextBlock.Text = "Invalid data provided";
+        //    }
+        //}
 
-        private bool HasSufficientFunds(string bankAccount, Loan loan)
-        {
-            // Implement your logic to check if the bank account has sufficient funds to pay the loan
-            return true; // Example logic
-        }
+        //private void PayLoanButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (PayBankAccountComboBox.SelectedIndex == -1 || LoanToPayComboBox.SelectedIndex == -1)
+        //    {
+        //        PayErrorMessageTextBlock.Text = "Invalid data provided";
+        //        return;
+        //    }
 
-        private void PayLoan(string bankAccount, Loan loan)
-        {
-            // Implement your logic to pay the loan and update the bank account balance
-        }
+        //    var selectedBankAccount = PayBankAccountComboBox.SelectedItem.ToString();
+        //    var selectedLoan = (Loan)LoanToPayComboBox.SelectedValue;
 
-        private void SaveLoan()
-        {
-            // Implement your loan saving logic here
-        }
+        //    if (HasSufficientFunds(selectedBankAccount, selectedLoan))
+        //    {
+        //        PayLoan(selectedBankAccount, selectedLoan);
+        //        ShowPage("MainLoansPage");
+        //        ClearPayLoanPage();
+        //    }
+        //    else
+        //    {
+        //        PayErrorMessageTextBlock.Text = "Not enough funds";
+        //    }
+        //}
+
+        //private bool HasSufficientFunds(string bankAccount, Loan loan)
+        //{
+        //    // Implement your logic to check if the bank account has sufficient funds to pay the loan
+        //    return true; // Example logic
+        //}
+
+        //private void PayLoan(string bankAccount, Loan loan)
+        //{
+        //    // Implement your logic to pay the loan and update the bank account balance
+        //}
+
+        //private void SaveLoan()
+        //{
+        //    // Implement your loan saving logic here
+        //}
     }
 }
--- LoanShark/LoanShark/ViewModel/LoanViewModel.cs
+++ LoanShark/LoanShark/ViewModel/LoanViewModel.cs
@@ -1,16 +1,246 @@
-﻿using System;
+﻿using LoanShark.Domain;
+using Microsoft.UI.Xaml;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Diagnostics;
+
 
 namespace LoanShark.ViewModel
 {
-    class LoanViewModel
+    public class LoanViewModel : INotifyPropertyChanged
     {
+        private int userID;
+        private string selectedBankAccount;
+        private Loan selectedLoan;
+        private float amount;
+        private int selectedMonths;
+        private string errorMessage;
+        private string payErrorMessage;
+        private string currentPage;
+
+        public ObservableCollection<Loan> Loans { get; set; }
+        public ObservableCollection<string> BankAccounts { get; set; }
+        public ObservableCollection<int> Months { get; set; }
+        public ObservableCollection<Loan> UnpaidLoans { get; set; }
+
+        public string SelectedBankAccount
+        {
+            get => selectedBankAccount;
+            set
+            {
+                selectedBankAccount = value;
+                OnPropertyChanged(nameof(SelectedBankAccount));
+                UpdateAmountToPay();
+            }
+        }
+
+        public Loan SelectedLoan
+        {
+            get => selectedLoan;
+            set
+            {
+                selectedLoan = value;
+                OnPropertyChanged(nameof(SelectedLoan));
+            }
+        }
+
+        public float Amount
+        {
+            get => amount;
+            set
+            {
+                amount = value;
+                OnPropertyChanged(nameof(Amount));
+                UpdateAmountToPay();
+            }
+        }
+
+        public int SelectedMonths
+        {
+            get => selectedMonths;
+            set
+            {
+                selectedMonths = value;
+                OnPropertyChanged(nameof(SelectedMonths));
+                UpdateAmountToPay();
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set
+            {
+                errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+
+        public string PayErrorMessage
+        {
+            get => payErrorMessage;
+            set
+            {
+                payErrorMessage = value;
+                OnPropertyChanged(nameof(PayErrorMessage));
+            }
+        }
+
+        public string CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+                UpdatePageVisibility();
+            }
+        }
+
+        private Visibility mainLoansPageVisibility;
+        public Visibility MainLoansPageVisibility
+        {
+            get => mainLoansPageVisibility;
+            set
+            {
+                mainLoansPageVisibility = value;
+                OnPropertyChanged(nameof(MainLoansPageVisibility));
+            }
+        }
+
+        private Visibility takeLoanPageVisibility;
+        public Visibility TakeLoanPageVisibility
+        {
+            get => takeLoanPageVisibility;
+            set
+            {
+                takeLoanPageVisibility = value;
+                OnPropertyChanged(nameof(TakeLoanPageVisibility));
+            }
+        }
+
+        private Visibility payLoanPageVisibility;
+        public Visibility PayLoanPageVisibility
+        {
+            get => payLoanPageVisibility;
+            set
+            {
+                payLoanPageVisibility = value;
+                OnPropertyChanged(nameof(PayLoanPageVisibility));
+            }
+        }
+
+        public ICommand TakeLoanCommand { get; }
+        public ICommand PayLoanCommand { get; }
+        public ICommand BackCommand { get; }
+        public ICommand GoToTakeLoanPageCommand { get; }
+        public ICommand GoToPayLoanPageCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public LoanViewModel(int userID)
         {
+            this.userID = userID;
+            Loans = new ObservableCollection<Loan>(GetUnpaidLoans().OrderBy(loan => loan.DateTaken));
+            BankAccounts = new ObservableCollection<string>(GetBankAccounts());
+            Months = new ObservableCollection<int> { 6, 12, 24, 36 };
+            UnpaidLoans = new ObservableCollection<Loan>(GetUnpaidLoans());
 
+            TakeLoanCommand = new RelayCommand(TakeLoan);
+            PayLoanCommand = new RelayCommand(PayLoan);
+            BackCommand = new RelayCommand(Back);
+            GoToTakeLoanPageCommand = new RelayCommand(() => CurrentPage = "TakeLoanPage");
+            GoToPayLoanPageCommand = new RelayCommand(() => CurrentPage = "PayLoanPage");
+
+            CurrentPage = "MainLoansPage";
+            UpdatePageVisibility();
+            Debug.WriteLine(MainLoansPageVisibility.ToString());
+            Debug.WriteLine(TakeLoanPageVisibility.ToString());
+            Debug.WriteLine(PayLoanPageVisibility.ToString());
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private IEnumerable<string> GetBankAccounts()
+        {
+            return new List<string>
+            {
+                "IBAN1 - EUR - 1000",
+                "IBAN2 - USD - 2000",
+                "IBAN3 - GBP - 3000"
+            };
+        }
+
+        private IEnumerable<Loan> GetUnpaidLoans()
+        {
+            var allLoans = GetAllLoans();
+            return allLoans.Where(loan => loan.State == "unpaid");
+        }
+
+        private IEnumerable<Loan> GetAllLoans()
+        {
+            var Loans = new List<Loan>();
+            for (int i = 0; i < 5; i++)
+                Loans.Add(new Loan(i, i, i.ToString(), i, i));
+            return Loans;
+        }
+
+        private void UpdateAmountToPay()
+        {
+            // Implement logic to update amount to pay based on selected bank account, amount, and months
+        }
+
+        private void TakeLoan()
+        {
+            // Implement logic to take a loan
+        }
+
+        private void PayLoan()
+        {
+            // Implement logic to pay a loan
+        }
+
+        private void Back()
+        {
+            CurrentPage = "MainLoansPage";
+        }
+
+        private void UpdatePageVisibility()
+        {
+            MainLoansPageVisibility = CurrentPage == "MainLoansPage" ? Visibility.Visible : Visibility.Collapsed;
+            TakeLoanPageVisibility = CurrentPage == "TakeLoanPage" ? Visibility.Visible : Visibility.Collapsed;
+            PayLoanPageVisibility = CurrentPage == "PayLoanPage" ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
         }
     }
 }
