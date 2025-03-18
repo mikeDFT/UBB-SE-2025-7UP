1030457d83782ea6fa98e6b1909ef2e2045a4d08
alexapvl <alex.robite@gmail.com>
2025-03-18 18:57:25 +0200

    added login service

---
+++ LoanShark/LoanShark/Service/LoginService.cs
@@ -0,0 +1,43 @@
+﻿using LoanShark.Domain;
+using LoanShark.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanShark.Service
+{
+    class LoginService
+    {
+        public LoginRepository repo;
+
+        public LoginService()
+        {
+            this.repo = new LoginRepository();
+        }
+
+        public bool ValidateUserCredentials(string email, string password)
+        {
+            try
+            {
+                DataTable dt = this.repo.GetUserCredentials(email);
+
+                //if exception is not thorwn, then the user exists and we continue with the validation
+                string hashedPassword = dt.Rows[0]["hashed_password"].ToString();
+                string passwordSalt = dt.Rows[0]["password_salt"].ToString();
+
+                HashedPassword userPassword = new HashedPassword(hashedPassword, passwordSalt, false);
+                HashedPassword inputPassword = new HashedPassword(password, passwordSalt, true);
+
+                return userPassword.Equals(inputPassword);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
