fd6869b37efd038746c4cc18e7717d5f980b089c
rilden <9265760+rilden@users.noreply.github.com>
2025-03-19 22:51:44 +0200

    Create UserService class

---
+++ LoanShark/LoanShark/Service/UserService.cs
@@ -0,0 +1,19 @@
+﻿using LoanShark.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanShark.Service
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+    }
+}
