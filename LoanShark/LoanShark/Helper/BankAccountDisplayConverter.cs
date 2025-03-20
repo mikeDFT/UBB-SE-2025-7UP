ea60f5706e2637fa76bba3d5d81f5d358d696c45
brain4ctivity <alexandru.alupei@stud.ubbcluj.ro>
2025-03-21 01:25:21 +0200

    Added a helper class for converting binded bank account to displayed string

---
+++ LoanShark/LoanShark/Helper/BankAccountDisplayConverter.cs
@@ -0,0 +1,30 @@
+﻿using LoanShark.Domain;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoanShark.Domain;
+
+namespace LoanShark.Helper
+{
+    public class BankAccountDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is BankAccount account)
+            {
+                return string.IsNullOrEmpty(account.name)
+                    ? $"{account.iban} ({account.currency})"
+                    : account.name;
+            }
+            return "Unknown Account";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
