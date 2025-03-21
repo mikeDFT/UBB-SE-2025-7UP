90b7b3fdd946ecb3921dc13f020e13bf60511429
alexapvl <alex.robite@gmail.com>
2025-03-21 23:44:36 +0200

    ascending order by date_created for the main page

--- LoanShark/LoanShark/Data/Procedures/BankAccounts/GetUserBankAccounts_Alex.sql
+++ LoanShark/LoanShark/Data/Procedures/BankAccounts/GetUserBankAccounts_Alex.sql
@@ -2,5 +2,5 @@ CREATE OR ALTER PROCEDURE GetUserBankAccounts
     @id_user int
 AS
 BEGIN
-    SELECT * FROM bank_accounts WHERE id_user = @id_user;
+    SELECT * FROM bank_accounts WHERE id_user = @id_user ORDER BY date_created ASC;
 END;
